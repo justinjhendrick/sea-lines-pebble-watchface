1a4cdec633b6134a41ffcc163025d62258a4d17a
Lavender Glaab <piggehperson@gmail.com>
2026-04-16 18:17:51 -0400

    Update pebble-clay to @rebble/clay

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,7 +1,7 @@
-var Clay = require('pebble-clay');
-var clayConfig = require('./config.json');
+var Clay = require("@rebble/clay");
+var clayConfig = require("./config.json");
 var clay = new Clay(clayConfig);
 
-Pebble.addEventListener('ready', function() {
-  console.log('PebbleKit JS ready!');
+Pebble.addEventListener("ready", function () {
+  console.log("PebbleKit JS ready!");
 });
